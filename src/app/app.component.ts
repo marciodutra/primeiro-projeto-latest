14b829e9dbfc67688956ce337df2ee43b821f04a
marciodutra <78557074+marciodutra@users.noreply.github.com>
2024-06-17 15:15:48 -0300

    app-template-deferrable-views

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,17 +4,19 @@ import { RouterOutlet } from '@angular/router';
 import { NewComponent } from './components/new-component/new-component.component';
 import { TemplateVariablesComponent } from './components/template/template-variables/template-variables.component';
 import { TemplateControlFlowComponent } from './components/template/template-control-flow/template-control-flow.component';
+import { TemplateDeferrableViewsComponent } from './components/template/template-deferrable-views/template-deferrable-views.component';
 
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NewComponent, NewComponent, TemplateVariablesComponent, TemplateControlFlowComponent],
+  imports: [RouterOutlet, NewComponent, NewComponent, TemplateVariablesComponent,
+     TemplateControlFlowComponent, TemplateDeferrableViewsComponent],
 
   template: `
 
   <h1>Curso de Angular</h1>
-  <app-template-control-flow />
+  <app-template-deferrable-views />
   `,
 })
 export class AppComponent {}
--- src/app/components/template/template-control-flow/template-control-flow.component.ts
+++ src/app/components/template/template-control-flow/template-control-flow.component.ts
@@ -5,20 +5,10 @@ import { Observable, delay, of } from 'rxjs';
 @Component({
   selector: 'app-template-control-flow',
   standalone: true,
-  imports: [AsyncPipe, CommonModule, NgIf, NgFor],
+  imports: [AsyncPipe, NgIf, NgFor],
   templateUrl: './template-control-flow.component.html',
   styleUrl: './template-control-flow.component.scss'
 })
 export class TemplateControlFlowComponent {
   public isTrue = true;
-
-  public loadingData$: Observable<string[]> = of([
-    'item 1',
-    'item 2',
-    'item 3',
-  ]).pipe(delay(3000));
-
-  public trackByFn(index: number){
-    return index;
-  }
 }
